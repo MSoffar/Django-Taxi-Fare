cfb1b57ae4cb34fe666f6f27bbc7ccedfaddda3a
Mohamed Soffar <159565280+MSoffar@users.noreply.github.com>
2024-08-08 23:33:58 +0300

    Delete myproject directory

--- myproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from django.shortcuts import redirect
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls')),
-    path('', lambda request: redirect('predict')),  # Default redirect to predict page
-]
